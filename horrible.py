713c5397aac5673bf23ccbeda6c69329f12f8583
amaanooo <md.amanuddin@gmail.com>
2018-12-06 20:05:31 -0500

    horrible

--- horrible.py
+++ horrible.py
@@ -36,6 +36,35 @@ def qu(node,a,b,i,j):
  q2=qu(node*2+1,(a+b)//2  +1,b,i,j)
  return q1+q2
 
+def up(node,a,b,i,j):
+ if a>b:
+  return
+ if l[node]!=0:
+  t[node]+=l[node]*(b-a+1)
+  if a!=b:
+   l[node*2]+=l[node]
+   l[node*2+1]+=l[node]
+   
+  l[node]=0
+  
+  if a>b or a>j or b < i:
+   return
+  if a>=i and b<=j:
+   t[node]+=inc*(b-a+1)
+   if a!=b:
+    l[node*2]+=inc
+    l[node*2+1]+=inc
+	
+   return
+   up(node*2,a,(a+b)//2,i,j,inc)
+   up(node*2+1(a+b)//2  +1,i,j,inc)
+   t[node]=t[node*2] + t[node*2+1]
+  
+  
+  
+  
+  
+ 
 
 
 if __name__=="__main__":
@@ -52,7 +81,13 @@ if __name__=="__main__":
         if x==1:
             p=int(input())
             q=int(input())
-        print(qu(1,0,n-1,p-1,q-1))
+	    print(qu(1,0,n-1,p-1,q-1))
+	else:
+	    p=int(input())
+	    q=int(input())
+	    inc=int(input())
+	    up(1,0,n-1,p-1,q-1,inc)
+		print(qu(1,0,n-1,p-1,q-1))
         ni=ni-1
         #push
 
